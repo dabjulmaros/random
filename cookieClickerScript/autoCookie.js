3b640904a53cd4e72ca686ef8aef065ffb1e0bcf
Dabjulmaros <33079720+dabjulmaros@users.noreply.github.com>
2023-12-28 07:22:59 -0500

    autocookie autoAscend

--- cookieClickerScript/autoCookie.js
+++ cookieClickerScript/autoCookie.js
@@ -1,32 +1,35 @@
-document.querySelector("#bigCookie").click();
+// document.querySelector("#bigCookie").click();
 
-Game.ClickCookie();
+// Game.ClickCookie();
 
-Game.CanClick;
+// Game.CanClick;
 
-Game.UpgradesInStore[0].click();
+// Game.UpgradesInStore[0].click();
 
-document
-  .querySelectorAll(".product.unlocked.enabled")
-  .forEach((e) => e.click());
+// document
+//   .querySelectorAll(".product.unlocked.enabled")
+//   .forEach((e) => e.click());
 
-Game.Ascend(1);
+// Game.Ascend(1);
 
-let run = true;
+let autoRun = true;
+let autoAscend = true;
 
-function auto() {
+function _auto() {
+  // id="ascend"
+  // id="ascendUpgrades" children
   if (Game.CanClick) {
     //click big cookie
     Game.ClickCookie();
 
     //buy new generators
-    [...document.querySelectorAll(".product.unlocked.enabled")].forEach((e) =>
-      e.click()
+    [...document.querySelectorAll("#store .product.unlocked.enabled")].forEach(
+      (e) => Game.ObjectsById[e.id.split("product")[1]].buy(1)
     );
 
     //buy upgrades
-    [...document.querySelectorAll(".crate.upgrade.enabled")].forEach((e) =>
-      e.click()
+    [...document.querySelectorAll("#store .crate.upgrade.enabled")].forEach(
+      (e) => Game.UpgradesById[e.dataset.id].buy(1)
     );
 
     // click on special cookies
@@ -41,53 +44,68 @@ function auto() {
     // document.querySelectorAll(".row.enabled .productButton.productLevel.lumpsOnly").forEach(e=>e.click())
   }
 
-  if (run) {
-    window.requestAnimationFrame(auto);
-  }
-}
-
-if (gainLumps) {
-  Game.gainLumps(10000);
-  let lastLumpCount = Game.lumps;
-  let spendLumpsBool = true;
-
-  function spendLumps() {
-    // upgrade all generators
-
-    [
-      ...document.querySelectorAll(".productButtons .productLevel.lumpsOnly"),
-    ].forEach((e) => e.click());
-    if (lastLumpCount == Game.lumps) spendLumpsBool = false;
-    if (spendLumpsBool) {
-      lastLumpCount = Game.lumps;
-      window.setTimeout(spendLumps, 1000);
-    }
-  }
-  spendLumps();
-}
-
-if (ascend) {
-  let lastAscendLength = [
-    ...document.querySelectorAll(".crate.upgrade.heavenly"),
-  ].length;
-  let spendAscendBool = true;
-  function spendAscend() {
-    [...document.querySelectorAll(".crate.upgrade.heavenly")].forEach((e) =>
-      e.click()
-    );
-    if (
-      lastAscendLength ==
-      [...document.querySelectorAll(".crate.upgrade.heavenly")].length
-    )
-      spendAscendBool = false;
-    if (spendAscendBool) {
-      lastAscendLength = [
-        ...document.querySelectorAll(".crate.upgrade.heavenly"),
-      ].length;
-      window.setTimeout(spendAscend, 1000);
+  if (autoRun) {
+    if (autoAscend) {
+      autoRun = false;
+      _ascend();
     } else {
-      Game.Reincarnate(1);
+      window.requestAnimationFrame(_auto);
     }
   }
-  spendAscend();
+}
+
+// if (gainLumps) {
+//   Game.gainLumps(10000);
+//   let lastLumpCount = Game.lumps;
+//   let spendLumpsBool = true;
+
+//   function _spendLumps() {
+//     // upgrade all generators
+
+//     [
+//       ...document.querySelectorAll(".productButtons .productLevel.lumpsOnly"),
+//     ].forEach((e) => e.click());
+//     if (lastLumpCount == Game.lumps) spendLumpsBool = false;
+//     if (spendLumpsBool) {
+//       lastLumpCount = Game.lumps;
+//       window.setTimeout(_spendLumps, 1000);
+//     }
+//   }
+//   _spendLumps();
+// }
+
+function _ascend() {
+  Game.Ascend(1);
+  window.setTimeout(_getAscend, 6000);
+  function _getAscend() {
+    let lastAscendLength = [
+      ...document.querySelectorAll(".crate.upgrade.heavenly"),
+    ].length;
+    console.log(lastAscendLength);
+    let spendAscendBool = true;
+    function _spendAscend() {
+      [
+        ...document.querySelectorAll(
+          ".crate.upgrade.heavenly:not(.ghosted):not(.enabled)"
+        ),
+      ].forEach((e) => e.click());
+      if (
+        lastAscendLength ==
+        [...document.querySelectorAll(".crate.upgrade.heavenly")].length
+      )
+        spendAscendBool = false;
+      if (spendAscendBool) {
+        lastAscendLength = [
+          ...document.querySelectorAll(".crate.upgrade.heavenly"),
+        ].length;
+        window.setTimeout(_spendAscend, 500);
+      } else {
+        autoAscend = false;
+        autoRun = true;
+        // Game.Reincarnate(1);
+        // window.setTimeout(_auto, 2000);
+      }
+    }
+    _spendAscend();
+  }
 }
