6ea6af9680a9d716f6338e92288d2733d6c6dc2d
Dabjulmaros <33079720+dabjulmaros@users.noreply.github.com>
2024-06-06 22:34:20 -0400

    format

--- lemmy.js
+++ lemmy.js
@@ -1,7 +1,7 @@
 // Fediverse helper script
-// One of the biggest issues right now is finding content in 
+// One of the biggest issues right now is finding content in
 // instances other than your home one
-// this script should take the current page found and open it on 
+// this script should take the current page found and open it on
 // the search of the defined home instance.
 
 // params = new URLSearchParams({
@@ -10,7 +10,6 @@
 // });
 
 // fetch(`//[Home Instance]}/api/v3/resolve_object?${params.toString()}`).then(r=>r.json()).then(j=>console.log(`//${window.location.host}/post/${j.post.counts.post_id}`);
-
 
 // Issue with that script is cors and fetching from the foreing instance
 
@@ -23,20 +22,16 @@
 //https://developer.chrome.com/docs/extensions/mv3/getstarted/tut-tabs-manager/
 //https://chrome.google.com/webstore/detail/custom-new-tab-url/mmjbdbjnoablegbkcklggeknkfcjkjia
 
-if(document.querySelectorAll("#root > .lemmy-site").length)
-  findAtHome();
+if (document.querySelectorAll("#root > .lemmy-site").length) findAtHome();
 
 function findAtHome(url = window.location.href) {
-  const home = '//reddthat.com'
-  if (url.includes(home))
-    return;
+  const home = "//reddthat.com";
+  if (url.includes(home)) return;
 
-  url = url.split('?')[0]
+  url = url.split("?")[0];
 
-  if (url.includes("/post/"))
-    findPostAtHome(home, url)
-  else if (url.includes('/c/'))
-    findCommAtHome(home);
+  if (url.includes("/post/")) findPostAtHome(home, url);
+  else if (url.includes("/c/")) findCommAtHome(home);
 
   // works if popups are allowed
   // window.open(homeSearch + param.toString(), '_blank', 'noopener, noreferrer');
@@ -45,35 +40,34 @@ function findAtHome(url = window.location.href) {
 function findPostAtHome(home, url) {
   const param = new URLSearchParams({
     q: url,
-    type: "Url"
-  })
+    type: "Url",
+  });
 
-  const ancher = document.createElement('a');
-  ancher.textContent = "Find @ Home"
-  ancher.target = "_blank"
-  ancher.style.marginLeft = ".5rem"
-  ancher.rel = 'noopener, noreferrer';
+  const ancher = document.createElement("a");
+  ancher.textContent = "Find @ Home";
+  ancher.target = "_blank";
+  ancher.style.marginLeft = ".5rem";
+  ancher.rel = "noopener, noreferrer";
   ancher.href = home + "/search?" + param.toString();
 
-  [...document.querySelectorAll('.post-title a')].forEach(e => {
+  [...document.querySelectorAll(".post-title a")].forEach((e) => {
     e.parentNode.appendChild(ancher.cloneNode(true));
-  })
+  });
 }
 
 function findCommAtHome(home) {
-  const alertBox = document.querySelector('.alert code.user-select-all')
-  const postLink = document.querySelector('.mb-2 .community-link');
+  const alertBox = document.querySelector(".alert code.user-select-all");
+  const postLink = document.querySelector(".mb-2 .community-link");
 
   const comm = alertBox.textContent.substring(1);
 
-  const ancher = document.createElement('a');
-  ancher.textContent = "Find @ Home"
-  ancher.target = "_blank"
-  ancher.style.marginLeft = ".5rem"
-  ancher.rel = 'noopener, noreferrer';
+  const ancher = document.createElement("a");
+  ancher.textContent = "Find @ Home";
+  ancher.target = "_blank";
+  ancher.style.marginLeft = ".5rem";
+  ancher.rel = "noopener, noreferrer";
   ancher.href = home + "/c/" + comm;
 
   alertBox.parentNode.appendChild(ancher.cloneNode(true));
   postLink.parentNode.appendChild(ancher.cloneNode(true));
-
 }
