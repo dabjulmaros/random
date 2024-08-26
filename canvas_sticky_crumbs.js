6a48e36b660f64e532c190ec1e3cf661f4863f7b
dabjulmaros <33079720+dabjulmaros@users.noreply.github.com.>
2024-08-25 21:29:26 -0400

    added canvas sticky headers

---
+++ canvas_sticky_crumbs.js
@@ -0,0 +1,24 @@
+/*
+div#wrapper > div.ic-app-nav-toggle-and-crumbs {
+  position: sticky;
+  top: 0;
+  background-color: var(--primary-contrast);
+  z-index: 100;
+}
+div#left-side > div#sticky-container {
+  top: 4.5rem;
+}
+*/
+(() => {
+  const styleEle = document.createElement('style');
+  styleEle.innerHTML = `div#wrapper > div.ic-app-nav-toggle-and-crumbs {
+  position: sticky;
+  top: 0;
+  background-color: var(--primary-contrast);
+  z-index: 100;
+}
+div#left-side > div#sticky-container {
+  top: 4.5rem;
+}`
+  document.head.appendChild(styleEle)
+})()
