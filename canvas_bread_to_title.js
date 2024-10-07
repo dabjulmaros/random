6d50b3f496d865de31bd4e272cb5759a428e18b4
dabjulmaros <33079720+dabjulmaros@users.noreply.github.com.>
2024-10-06 23:23:50 -0400

    test firefox extension

--- canvas_bread_to_title.js
+++ canvas_bread_to_title.js
@@ -3,6 +3,16 @@
 (() => {
   const bread = document.querySelectorAll("#breadcrumbs li");
   let title = bread[1].innerText.match(/\w{3,}\d{4}/)[0];
-  if (bread.length >= 3) title += " " + bread[2].innerText;
+  if (bread.length >= 4 && !bread[3].innerHTML.includes(ENV.current_user_id)) {
+    title += " " + bread[3].innerText;
+  }
+  else if (bread.length >= 3) {
+    title += " " + bread[2].innerText;
+  }
   document.title = title;
 })();
+
+
+// javascript: (() => {const bread = document.querySelectorAll("#breadcrumbs li");let title = bread[1].innerText.match(/\w{3,}\d{4}/)[0];if (bread.length >= 4 && !bread[3].innerHTML.includes(ENV.current_user_id)) {title += " " + bread[3].innerText;}else if (bread.length >= 3) {title += " " + bread[2].innerText;}document.title = title;})();
+
+
