e5aef250f8d4ddba66cac14ffcd318da6ce2654d
Dabjulmaros <33079720+dabjulmaros@users.noreply.github.com>
2025-09-01 09:16:51 -0400

    notes mvp

--- noteCopyExtension/action/script.js
+++ noteCopyExtension/action/script.js
@@ -1,17 +1,32 @@
+const notes = [];
+
+chrome.storage.local.get(["myNotes"]).then((data) => {
+  notes.push(...data.myNotes)
+  for (const i in notes) {
+    createItem(notes[i][0], notes[i][1], i);
+  }
+});
+
 const wrapper = document.getElementById('wrapper');
 
 const dialog = document.querySelector('dialog');
 const newTitle = document.getElementById('newTitle');
 const newNote = document.getElementById('newNote');
 
-// chrome.storage.local.get(["home"]).then((data) => {
-//   if (data.home)
-//     document.querySelector('input').value = data.home;
-// });
+document.querySelector('button[title="Create Note"]').onclick = () => addNote();
+document.querySelector('button[title="Cancel"]').onclick = () => submitNote(document.querySelector('button[title="Cancel"]'));
+document.querySelector('button[title="Add"]').onclick = () => submitNote(document.querySelector('button[title="Add"]'));
 
-// document.getElementById("save").addEventListener("click", (e) => {
-//   chrome.storage.local.set({ home: document.querySelector('input').value })
-// })
+const popout = document.getElementById("popout");
+if (location.href.includes("popout")) {
+  popout.remove();
+  document.title = "Copy Quick Notes"
+} else {
+  popout.addEventListener("click", (e) => {
+    window.open(location.href + "#popout", "", 'popup,menubar=no,location=no,toolbar=no,noopener=no,noreferrer=no,resizable=no,width=550,height=650')
+  })
+}
+
 
 function copyText(ele) {
   const textArea = ele.parentElement.querySelector('textarea');
@@ -21,15 +36,24 @@ function copyText(ele) {
 function editText(ele) {
   const textArea = ele.parentElement.querySelector('textarea');
   textArea.disabled = !textArea.disabled
+  if (textArea.disabled) {
+    notes[ele.parentElement.getAttribute('noteIndex')][1] = textArea.value;
+    storeNotes();
+  }
 }
 
 function addNote() {
   dialog.showModal();
 }
+function storeNotes() {
+  chrome.storage.local.set({ myNotes: notes });
+}
 
 function submitNote(ele) {
   if (ele.title === "Add") {
     createItem(newTitle.value, newNote.value);
+    notes.push([newTitle.value, newNote.value])
+    storeNotes();
   }
   newTitle.value = "";
   newNote.value = "";
@@ -37,7 +61,7 @@ function submitNote(ele) {
 }
 
 
-function createItem(_title, _value) {
+function createItem(_title, _value, _index) {
   const section = document.createElement('div');
   section.classList.add("section");
 
@@ -46,6 +70,8 @@ function createItem(_title, _value) {
 
   const item = document.createElement('div');
   item.classList.add('item');
+  item.setAttribute("noteIndex", _index)
+  console.log(_index)
 
   const textArea = document.createElement('textarea');
   textArea.disabled = true;
