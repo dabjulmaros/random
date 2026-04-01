561c7694cd16977f063a998d89ccd1f257895346
dabjulmaros <33079720+dabjulmaros@users.noreply.github.com.>
2026-04-01 03:12:07 -0400

    audible in dark mode

---
+++ AudibleDarkMode/scripts/content.js
@@ -0,0 +1,23 @@
+
+chrome.storage.local.get(["AudibleDark"]).then((result) => {
+  const dark = result.AudibleDark
+  console.log("My Log: ", dark)
+  if (dark) {
+    document.querySelectorAll('body,.bc-tray,.bc-box,.bc-color-base').forEach(e => {
+      e.style.color = "#fff";
+      e.style.backgroundColor = "#262626";
+    });
+    document.querySelectorAll('#adbl-cloud-player-controls img').forEach(e => e.style.filter = 'invert(0.85)')
+    document.querySelectorAll('.bc-text,.bc-icon.bc-icon-fill-base').forEach(e => e.style.color = '#cbcbcb')
+    document.querySelectorAll('.bc-box .bc-icon-fill-base:not(.bc-icon-bookmarks,#adbl-cloud-player-menu-icon)').forEach(e => { e.style.backgroundColor = '#cbcbcb' })
+  }
+});
+
+function setAudibleDark(value) {
+  if (value !== true || value !== false)
+    return "Invalid Input"
+
+  chrome.storage.local.set({ AudibleDark: val });
+  return "Success"
+}
+
